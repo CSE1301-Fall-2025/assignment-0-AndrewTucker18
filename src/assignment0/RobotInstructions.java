5585d9a9aec7d0bfd1238e385465065dae90bc2f
AndrewTucker18 <a.j.tucker@wustl.edu>
2025-09-03 09:44:48 -0500

    Finished Assignment

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -31,9 +31,15 @@ public class RobotInstructions  {
 		 */
 		robot.forward(60);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(300);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(300);
+		robot.turnRight(90);
+		robot.forward(300);
+		robot.turnRight(90);
+		robot.forward(360);
+
+		
 	}
 
 }
